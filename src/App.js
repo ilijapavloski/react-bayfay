63f0058756e58ffdb23cd65d6c96d35538149e3d
Dev <dev@Devs-MacBook-Pro.local>
2021-04-12 07:12:06 +0300

    fixed error

--- src/App.js
+++ src/App.js
@@ -103,13 +103,13 @@ function App({location}) {
         if (window.location.pathname === '/merchant-policy' || window.location.pathname === '/privacy' || window.location.pathname === '/terms') {
             return;
         }
-        if (/android/i.test(userAgent)) {
-            window.location.href = 'https://play.google.com/store/apps/details?id=com.bayfay.customer';
-        }
+        // if (/android/i.test(userAgent)) {
+        //     window.location.href = 'https://play.google.com/store/apps/details?id=com.bayfay.customer';
+        // }
 
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            window.location.href = 'https://apps.apple.com/in/app/bayfay-shop-from-any-shops/id1463215060';
-        }
+        // if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        //     window.location.href = 'https://apps.apple.com/in/app/bayfay-shop-from-any-shops/id1463215060';
+        // }
     };
 
     useEffect(() => {
